bf5a020c82c0cfdab05676435495656ffc3e3c52
snoweye <wccsnow@gmail.com>
2014-06-02 09:47:03 -0400

    minor

--- R/windows/zzz.R
+++ R/windows/zzz.R
@@ -9,21 +9,35 @@
 # } # End of .Last.lib().
 
 .onLoad <- function(libname, pkgname){
-  ### Modify from "../get.conf.r"
-  file.name <- paste("./etc/", .Platform$r_arch, "/Makeconf", sep = "")
-  file.path <- tools::file_path_as_absolute(
-                 system.file(file.name, package = "pbdNCDF4"))
-  ret <- scan(file.path, what = character(), sep = "\n", quiet = TRUE)
+  ### Check environment variables first, check Makeconf second.
+  netcdf4.root.32 <- Sys.getenv("NETCDF4_ROOT_32")
+  netcdf4.root.64 <- Sys.getenv("NETCDF4_ROOT_64")
 
-  ### Get NETCDF4_ROOT
-  arg <- "NETCDF4_ROOT"
-  id <- grep(paste("^", arg, " = ", sep = ""), ret)
-  netcdf4.root <- gsub(paste("^", arg, " = (.*)", sep = ""), "\\1", ret[id[1]])
+  if(netcdf4.root.32 != "" && .Platform$r_arch == "i386"){
+    netcdf4.root <- netcdf4.root.32
+    netcdf4.arch <- "w32"
+  } else if(netcdf4.root.64 != "" && .Platform$r_arch == "x64"){
+    netcdf4.root <- netcdf4.root.64
+    netcdf4.arch <- "x64"
+  } else{
+    ### Modify from "../get.conf.r"
+    file.name <- paste("./etc/", .Platform$r_arch, "/Makeconf", sep = "")
+    file.path <- tools::file_path_as_absolute(
+                   system.file(file.name, package = "pbdNCDF4"))
+    ret <- scan(file.path, what = character(), sep = "\n", quiet = TRUE)
 
-  ### Get NETCDF4_ARCH.
-  arg <- "NETCDF4_ARCH"
-  id <- grep(paste("^", arg, " = ", sep = ""), ret)
-  netcdf4.arch <- gsub(paste("^", arg, " = (.*)", sep = ""), "\\1", ret[id[1]])
+    ### Get NETCDF4_ROOT
+    arg <- "NETCDF4_ROOT"
+    id <- grep(paste("^", arg, " = ", sep = ""), ret)
+    netcdf4.root <- gsub(paste("^", arg, " = (.*)", sep = ""),
+                         "\\1", ret[id[1]])
+
+    ### Get NETCDF4_ARCH.
+    arg <- "NETCDF4_ARCH"
+    id <- grep(paste("^", arg, " = ", sep = ""), ret)
+    netcdf4.arch <- gsub(paste("^", arg, " = (.*)", sep = ""),
+                         "\\1", ret[id[1]])
+  }
 
   ### Add PATH and set NETCDF4_BIN to environment.
   netcdf4.bin <- paste(netcdf4.root, "bin/", sep = "")
